102ae41c9fe5d58ccecba6a2d79d6423a2baa961
Vamsi Krishna <46787868+mathalav55@users.noreply.github.com>
2025-01-03 14:16:26 +0530

    [WEB-2870]feat: language support (#6215)
    * fix: adding language support package
    
    * fix: language support implementation using mobx
    
    * fix: adding more languages for support
    
    * fix: profile settings translations
    
    * feat: added language support for sidebar and user settings
    
    * feat: added language support for deactivation modal
    
    * fix: added project sync after transfer issues (#6200)
    
    * code refactor and improvement (#6203)
    
    * chore: package code refactoring
    
    * chore: component restructuring and refactor
    
    * chore: comment create improvement
    
    * refactor: enhance workspace and project wrapper modularity (#6207)
    
    * [WEB-2678]feat: added functionality to add labels directly from dropdown (#6211)
    
    * enhancement:added functionality to add features directly from dropdown
    
    * fix: fixed import order
    
    * fix: fixed lint errors
    
    * chore: added common component for project activity (#6212)
    
    * chore: added common component for project activity
    
    * fix: added enum
    
    * fix: added enum for initiatives
    
    * - Do not clear temp files that are locked. (#6214)
    
    - Handle edge cases in sync workspace
    
    * fix: labels empty state for drop down (#6216)
    
    * refactor: remove cn helper function from the editor package (#6217)
    
    * * feat: added language support to issue create modal in sidebar
    * fix: project activity type
    
    * * fix: added missing translations
    * fix: modified translation for plurals
    
    * fix: fixed spanish translation
    
    * dev: language type error in space user profile types
    
    * fix: type fixes
    
    * chore: added alpha tag
    
    ---------
    
    Co-authored-by: sriram veeraghanta <veeraghanta.sriram@gmail.com>
    Co-authored-by: Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
    Co-authored-by: Prateek Shourya <prateekshourya29@gmail.com>
    Co-authored-by: Akshita Goyal <36129505+gakshita@users.noreply.github.com>
    Co-authored-by: Satish Gandham <satish.iitg@gmail.com>
    Co-authored-by: Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
    Co-authored-by: gurusinath <gurusainath007@gmail.com>

--- web/core/components/workspace/sidebar/help-section.tsx
+++ web/core/components/workspace/sidebar/help-section.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { observer } from "mobx-react";
 import { useParams } from "next/navigation";
 import { FileText, HelpCircle, MessagesSquare, MoveLeft, User } from "lucide-react";
+import { useTranslation } from "@plane/i18n";
 // ui
 import { CustomMenu, Tooltip, ToggleSwitch } from "@plane/ui";
 // components
@@ -24,6 +25,7 @@ export interface WorkspaceHelpSectionProps {
 export const SidebarHelpSection: React.FC<WorkspaceHelpSectionProps> = observer(() => {
   const { workspaceSlug, projectId } = useParams();
   // store hooks
+  const { t } = useTranslation();
   const { sidebarCollapsed, toggleSidebar } = useAppTheme();
   const { toggleShortcutModal } = useCommandPalette();
   const { isMobile } = usePlatformOS();
@@ -51,9 +53,120 @@ export const SidebarHelpSection: React.FC<WorkspaceHelpSectionProps> = observer(
           }
         )}
       >
-        
         <div
-          className={`flex flex-shrink-0 items-center gap-1 ${isCollapsed ? "flex-col justify-center" : "justify-end ml-auto"}`}
+          className={`relative flex flex-shrink-0 items-center gap-1 ${isCollapsed ? "flex-col justify-center" : "justify-evenly"}`}
+        >
+          <CustomMenu
+            customButton={
+              <div
+                className={cn(
+                  "grid place-items-center rounded-md p-1 outline-none text-custom-text-200 hover:text-custom-text-100 hover:bg-custom-background-90",
+                  {
+                    "bg-custom-background-90": isNeedHelpOpen,
+                  }
+                )}
+              >
+                <Tooltip tooltipContent="Help" isMobile={isMobile} disabled={isNeedHelpOpen}>
+                  <HelpCircle className="h-[18px] w-[18px] outline-none" />
+                </Tooltip>
+              </div>
+            }
+            customButtonClassName={`relative grid place-items-center rounded-md p-1.5 outline-none ${isCollapsed ? "w-full" : ""}`}
+            menuButtonOnClick={() => !isNeedHelpOpen && setIsNeedHelpOpen(true)}
+            onMenuClose={() => setIsNeedHelpOpen(false)}
+            placement={isCollapsed ? "left-end" : "top-end"}
+            maxHeight="lg"
+            closeOnSelect
+          >
+            <CustomMenu.MenuItem>
+              <a
+                href="https://go.plane.so/p-docs"
+                target="_blank"
+                className="flex items-center justify- gap-x-2 rounded text-xs hover:bg-custom-background-80"
+              >
+                <FileText className="h-3.5 w-3.5 text-custom-text-200" size={14} />
+                <span className="text-xs">{t("documentation")}</span>
+              </a>
+            </CustomMenu.MenuItem>
+            {config?.intercom_app_id && config?.is_intercom_enabled && (
+              <CustomMenu.MenuItem>
+                <button
+                  type="button"
+                  onClick={handleCrispWindowShow}
+                  className="flex w-full items-center gap-x-2 rounded text-xs hover:bg-custom-background-80"
+                >
+                  <MessagesSquare className="h-3.5 w-3.5 text-custom-text-200" />
+                  <span className="text-xs">{t("message_support")}</span>
+                </button>
+              </CustomMenu.MenuItem>
+            )}
+            <CustomMenu.MenuItem>
+              <a
+                href="mailto:sales@plane.so"
+                target="_blank"
+                className="flex items-center justify- gap-x-2 rounded text-xs hover:bg-custom-background-80"
+              >
+                <User className="h-3.5 w-3.5 text-custom-text-200" size={14} />
+                <span className="text-xs">{t("contact_sales")}</span>
+              </a>
+            </CustomMenu.MenuItem>
+            <div className="my-1 border-t border-custom-border-200" />
+            <CustomMenu.MenuItem>
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                }}
+                className="flex w-full items-center justify-between text-xs hover:bg-custom-background-80"
+              >
+                <span className="racking-tight">{t("hyper_mode")}</span>
+                <ToggleSwitch
+                  value={canUseLocalDB}
+                  onChange={() => toggleLocalDB(workspaceSlug?.toString(), projectId?.toString())}
+                />
+              </div>
+            </CustomMenu.MenuItem>
+            <CustomMenu.MenuItem>
+              <button
+                type="button"
+                onClick={() => toggleShortcutModal(true)}
+                className="flex w-full items-center justify-start text-xs hover:bg-custom-background-80"
+              >
+                <span className="text-xs">{t("keyboard_shortcuts")}</span>
+              </button>
+            </CustomMenu.MenuItem>
+            <CustomMenu.MenuItem>
+              <button
+                type="button"
+                onClick={() => setProductUpdatesModalOpen(true)}
+                className="flex w-full items-center justify-start text-xs hover:bg-custom-background-80"
+              >
+                <span className="text-xs">{t("whats_new")}</span>
+              </button>
+            </CustomMenu.MenuItem>
+            <CustomMenu.MenuItem>
+              <a
+                href="https://go.plane.so/p-discord"
+                target="_blank"
+                className="flex items-center justify- gap-x-2 rounded text-xs hover:bg-custom-background-80"
+              >
+                <span className="text-xs">Discord</span>
+              </a>
+            </CustomMenu.MenuItem>
+            <div className="px-1 pt-2 mt-1 text-xs text-custom-text-200 border-t border-custom-border-200">
+              <PlaneVersionNumber />
+            </div>
+          </CustomMenu>
+        </div>
+        <div
+          className={cn("w-full flex-grow px-0.5", {
+            hidden: isCollapsed,
+          })}
+        >
+          <WorkspaceEditionBadge />
+        </div>
+        <div
+          className={`flex flex-shrink-0 items-center gap-1 ${isCollapsed ? "flex-col justify-center" : "justify-evenly"}`}
         >
           <Tooltip tooltipContent={`${isCollapsed ? "Expand" : "Hide"}`} isMobile={isMobile}>
             <button
