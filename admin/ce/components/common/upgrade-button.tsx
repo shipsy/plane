d86177501391914a7c3124c187926ca455b74e72
Malleswari Segireddy <malleswaramma.segireddy@shipsy.io>
2025-06-04 11:38:00 +0530

    fixing the es-lint issues

--- admin/ce/components/common/upgrade-button.tsx
+++ admin/ce/components/common/upgrade-button.tsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 // icons
-import { SquareArrowOutUpRight } from "lucide-react";
+// import { SquareArrowOutUpRight } from "lucide-react";
 // ui
 // import { getButtonStyling } from "@plane/ui";
 // helpers
-import { cn } from "@/helpers/common.helper";
+// import { cn } from "@/helpers/common.helper";
 
 export const UpgradeButton: React.FC = () => (
-  <a></a>
+  <a />
   // <a href="https://plane.so/one" target="_blank" className={cn(getButtonStyling("primary", "sm"))}>
   //   Available on One
   //   <SquareArrowOutUpRight className="h-3.5 w-3.5 p-0.5" />
