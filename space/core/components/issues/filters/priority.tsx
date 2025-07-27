3e27e91bdef2e7643c81e7dc058f5efdf1bc991c
rsahay47 <rahul.sahay@shipsy.io>
2025-07-27 22:33:35 +0530

    Priority key translation

--- space/core/components/issues/filters/priority.tsx
+++ space/core/components/issues/filters/priority.tsx
@@ -29,7 +29,7 @@ export const FilterPriority: React.FC<Props> = observer((props) => {
   return (
     <>
       <FilterHeader
-        title={`${t("priority")}${appliedFiltersCount > 0 ? ` (${appliedFiltersCount})` : ""}`}
+        title={`${t("Priority")}${appliedFiltersCount > 0 ? ` (${appliedFiltersCount})` : ""}`}
         isPreviewEnabled={previewEnabled}
         handleIsPreviewEnabled={() => setPreviewEnabled(!previewEnabled)}
       />
