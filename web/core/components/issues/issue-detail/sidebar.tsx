9225a292ae522120d40e7396540f1221833cbc81
Harsh Yadav <harsh.yadav@shipsy.io>
2026-01-10 00:02:32 +0530

    created by by fixed

--- web/core/components/issues/issue-detail/sidebar.tsx
+++ web/core/components/issues/issue-detail/sidebar.tsx
@@ -174,7 +174,7 @@ export const IssueDetailsSidebar: React.FC<Props> = observer((props) => {
               <div className="flex h-8 items-center gap-2">
                 <div className="flex w-2/5 flex-shrink-0 items-center gap-1 text-sm text-custom-text-300">
                   <UserCircle2 className="h-4 w-4 flex-shrink-0" />
-                  <span>{t("created_by")} by</span>
+                  <span>{t("created_by")}</span>
                 </div>
                 <div className="w-full h-full flex items-center gap-1.5 rounded px-2 py-0.5 text-sm justify-between cursor-not-allowed">
                   <ButtonAvatars showTooltip userIds={createdByDetails.id} />
