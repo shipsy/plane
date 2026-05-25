9075cb105f3f95e187167ceb534d3fbf8564488d
Shipsy Kunal <kunalnandeshwar@Shipsys-MacBook-Air-7.local>
2026-05-25 13:37:35 +0530

    adding the activity section render part

---
+++ web/core/components/issues/issue-detail/issue-activity/activity/actions/start_date_time.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import { observer } from "mobx-react";
+import { CalendarClock } from "lucide-react";
+// hooks
+import { renderFormattedDate, renderFormattedTime } from "@/helpers/date-time.helper";
+import { useIssueDetail } from "@/hooks/store";
+// components
+import { IssueActivityBlockComponent, IssueLink } from "./";
+// helpers
+import { useTranslation } from "@plane/i18n";
+
+type TIssueStartDateTimeActivity = { activityId: string; showIssue?: boolean; ends: "top" | "bottom" | undefined };
+
+export const IssueStartDateTimeActivity: FC<TIssueStartDateTimeActivity> = observer((props) => {
+  const { activityId, showIssue = true, ends } = props;
+  const { t } = useTranslation();
+  const {
+    activity: { getActivityById },
+  } = useIssueDetail();
+
+  const activity = getActivityById(activityId);
+
+  if (!activity) return <></>;
+  return (
+    <IssueActivityBlockComponent
+      icon={<CalendarClock size={14} className="text-custom-text-200" aria-hidden="true" />}
+      activityId={activityId}
+      ends={ends}
+    >
+      <>
+        {activity.new_value
+          ? `${t("set_start_date_time_to", { defaultValue: "set the start date & time to" })} `
+          : `${t("removed_start_date_time", { defaultValue: "removed the start date & time" })} `}
+        {activity.new_value && (
+          <span className="font-medium text-custom-text-100">
+            {`${renderFormattedDate(activity.new_value)} ${renderFormattedTime(activity.new_value, "12-hour")}`}
+          </span>
+        )}
+        {showIssue && (activity.new_value ? ` ${t("for")} ` : ` ${t("from")} `)}
+        {showIssue && <IssueLink activityId={activityId} />}.
+      </>
+    </IssueActivityBlockComponent>
+  );
+});
